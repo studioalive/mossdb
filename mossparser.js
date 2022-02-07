7003fdb36310becde16569e113faacb55bb3449c
dorisdesk <96373586+studioalive@users.noreply.github.com>
2022-02-07 20:02:20 +0000

    wiki parsing if found

--- mossparser.js
+++ mossparser.js
@@ -31,9 +31,49 @@ function mossFilter() {
         (!gem || x.Gem === gem) &&
        (!light || x.L === parseInt(light)) &&
         (!form || x.LF1 === form) &&
-        (!ph || x.R === parseInt(ph));
+        (!ph || x.R === parseInt(ph)) &&
+        (!wet || x.F === parseInt(wet));
       });
 
     document.getElementById("mossNumber").innerHTML=mossList.length;
+    var title = mossList[0].Name_new;
+    console.log(title);
+    getWiki(title,0);
 }
 
+// function mossMaker(i) {
+// https://commons.wikimedia.org/w/api.php?action=query&generator=images&prop=imageinfo&gimlimit=8&redirects=1t&iiprop=url&format=json&titles=
+// }
+
+function getWiki(title, i) {
+
+                var regex = / /gm;
+                var str = title;
+                var subst = `_`;
+                var wiki = str.replace(regex, subst);
+
+    var apiEndpoint = "https://en.wikipedia.org/w/api.php";
+    var params = "action=parse&format=json&page=" + wiki;
+
+    fetch(apiEndpoint + "?" + params + "&origin=*")
+        .then(function (response) { return response.json(); })
+        .then(function (response) {
+            wikidata = response;
+            wikitext = wikidata.parse.text["*"];
+            console.log(wikitext);
+            var regex = /src="/gm;
+            var str = wikitext;
+            var subst = `src="https:`;
+            var result = str.replace(regex, subst);
+
+            var regex2 = /href="\/wiki/gm;
+            var str2 = result;
+            var subst2 = `target="_blank" href="https://en.wikipedia.org/wiki`;
+            var result2 = str2.replace(regex2, subst2);
+
+            var nodeP = document.createElement("DIV");
+            nodeP.id = "posttext" + i;
+            document.getElementById(i).appendChild(nodeP).innerHTML = result2;
+        });
+
+}
